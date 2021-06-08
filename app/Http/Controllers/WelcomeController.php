e88364a9dbebacce2c3807b64f8d445d7d246eae
Gilang <ondozwahyudi@gmail.com>
2021-06-08 23:03:02 +0700

    test cache

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -9,8 +9,13 @@ class WelcomeController extends Controller
 {
     public function index()
     {
-        $response = Http::get('https://api.kawalcorona.com/indonesia')->json();
-        $provinsi = Http::get('https://api.kawalcorona.com/indonesia/provinsi')->json();
+        $response =  cache()->remember('home', 60 * 60 * 60, function () {
+            return Http::get('https://api.kawalcorona.com/indonesia')->json();
+        });
+        $provinsi =  cache()->remember('home-provinsi', 60 * 60 * 60, function () {
+            return Http::get('https://api.kawalcorona.com/indonesia/provinsi')->json();
+        });
+
         return view('welcome', compact('response', 'provinsi'));
     }
 }
